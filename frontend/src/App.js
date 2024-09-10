bf6fb080eede2cb6d7f580a87040f5907ee2d6a6
calvinlee326 <calvinlee326@gmail.com>
2024-09-10 14:56:23 -0700

    Initial React setup and basic structure

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Portfolio</h1>
+        <p>This is the beginning of something awesome!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
